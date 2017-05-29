ecc9d740782fb35b63a40377f12ef870cad834c4
VitalieDuca <vtk.quasar@gmail.com>
2017-05-29 17:27:47 +0300

    ro.js
    Translation to Romanian language

---
+++ src/locale/lang/ro.js
@@ -0,0 +1,104 @@
+export default {
+  el: {
+    colorpicker: {
+      confirm: 'OK',
+      clear: 'Curăță'
+    },
+    datepicker: {
+      now: 'Acum',
+      today: 'Astăzi',
+      cancel: 'Anulează',
+      clear: 'Curăță',
+      confirm: 'OK',
+      selectDate: 'Alege data',
+      selectTime: 'Alege timpul',
+      startDate: 'Data de început',
+      startTime: 'Timpul de început',
+      endDate: 'Data de încheiere',
+      endTime: 'Timpul de încheiere',
+      year: 'An',
+      month1: 'Ianuarie',
+      month2: 'Februarie',
+      month3: 'Martie',
+      month4: 'Aprilie',
+      month5: 'Mai',
+      month6: 'Iunie',
+      month7: 'Iulie',
+      month8: 'August',
+      month9: 'Septembrie',
+      month10: 'Octombrie',
+      month11: 'Noiembrie',
+      month12: 'Decembrie',
+      // week: 'săptămână',
+      weeks: {
+        sun: 'Du',
+        mon: 'Lu',
+        tue: 'Ma',
+        wed: 'Mi',
+        thu: 'Jo',
+        fri: 'Vi',
+        sat: 'Sâ'
+      },
+      months: {
+        jan: 'Ian',
+        feb: 'Feb',
+        mar: 'Mart',
+        apr: 'Apr',
+        may: 'Mai',
+        jun: 'Iun',
+        jul: 'Iul',
+        aug: 'Aug',
+        sep: 'Sept',
+        oct: 'Oct',
+        nov: 'Nov',
+        dec: 'Dec'
+      }
+    },
+    select: {
+      loading: 'Încărcare',
+      noMatch: 'Nu există date potrivite',
+      noData: 'Nu sunt date',
+      placeholder: 'Selectare'
+    },
+    cascader: {
+      noMatch: 'Nu există date potrivite',
+      loading: 'Încărcare',
+      placeholder: 'Selectare'
+    },
+    pagination: {
+      goto: 'Mergi la',
+      pagesize: '/page',
+      total: 'Total {total}',
+      pageClassifier: ''
+    },
+    messagebox: {
+      title: 'Mesaj',
+      confirm: 'OK',
+      cancel: 'Anulează',
+      error: 'Date introduse invalide'
+    },
+    upload: {
+      delete: 'Șterge',
+      preview: 'Previzualizează',
+      continue: 'Continuă'
+    },
+    table: {
+      emptyText: 'Nu sunt date',
+      confirmFilter: 'Confirmă',
+      resetFilter: 'Resetează',
+      clearFilter: 'Toate',
+      sumText: 'Sumează'
+    },
+    tree: {
+      emptyText: 'Nu sunt date'
+    },
+    transfer: {
+      noMatch: 'Nu există date potrivite',
+      noData: 'Nu sunt date',
+      titles: ['Lista 1', 'Lista 2'],
+      filterPlaceholder: 'Introdu cuvântul-cheie',
+      noCheckedFormat: '{total} itemi',
+      hasCheckedFormat: '{checked}/{total} verificați'
+    }
+  }
+};
